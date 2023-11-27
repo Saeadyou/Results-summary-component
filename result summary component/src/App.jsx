0bf7d48e8a89e8f4fdae8d9ceba9d389185682f7
saeadyou <saeadyou@gmail.com>
2023-11-27 07:19:14 +0100

    add vite

---
+++ result summary component/src/App.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <p>
+        Your Result 76 of 100 Great You scored higher than 65% of the people who
+        have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
+        61 / 100 Visual 72 / 100 Continue
+      </p>
+      <div class="attribution">
+        Challenge by
+        <a
+          href="https://www.frontendmentor.io?ref=challenge"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Frontend Mentor
+        </a>
+        . Coded by <a href="https://github.com/Saeadyou">Mahdi Yousefi</a>.
+      </div>
+    </>
+  );
+}
+
+export default App;
