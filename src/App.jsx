5c668ce6b62f73f7aef40bd23fc86cd1acdde3ea
saeadyou <saeadyou@gmail.com>
2024-01-01 22:03:13 +0100

    create structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,37 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import "./App.css";
-
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <p>
-        Your Result 76 of 100 Great You scored higher than 65% of the people who
-        have taken these tests. Summary Reaction 80 / 100 Memory 92 / 100 Verbal
-        61 / 100 Visual 72 / 100 Continue
-      </p>
-      <div className="attribution">
+      <div className="flex">
+        <div>
+          <h3>Your Result</h3>
+          <div>
+            <span className="">76</span> of 100
+          </div>
+          <h2>Great</h2>
+          <p>
+            You scored higher than 65% of the people who have taken these tests.
+          </p>
+        </div>
+        <div>
+          <h2>Summary</h2>
+          <ul>
+            <li>
+              <span>Reaction</span> <span></span>80 / 100
+            </li>
+            <li>
+              <span>Memory</span> <span>92 / 100</span>
+            </li>
+            <li>
+              <span>Verbal</span> <span>61 / 100</span>
+            </li>
+            <li>
+              <span>Visual</span> <span>72 / 100</span>
+            </li>
+          </ul>
+          <button>Continue</button>
+        </div>
+      </div>
+      {/* <div>
         Challenge by
         <a
           href="https://www.frontendmentor.io?ref=challenge"
@@ -22,7 +41,7 @@ function App() {
           Frontend Mentor
         </a>
         . Coded by <a href="https://github.com/Saeadyou">Mahdi Yousefi</a>.
-      </div>
+      </div> */}
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js}"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx"],
   theme: {
     fontFamily: { sans: "Hanken Grotesk" },
+    fontSize: { base: ["18px", "27px"] },
     extend: {
       height: {
         screen: "100dvh",
