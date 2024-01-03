64655c5e53ad27aafd387180afc41b64eca9f377
saeadyou <saeadyou@gmail.com>
2024-01-03 15:31:54 +0100

    complete desktop view

--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,3 +1,8 @@
+import { HiOutlineBolt } from "react-icons/hi2";
+import { LiaBrainSolid } from "react-icons/lia";
+import { BiMessageRoundedMinus } from "react-icons/bi";
+import { TbEye } from "react-icons/tb";
+
 function Home() {
   return (
     <div className="grid w-[40rem] grid-cols-2 rounded-3xl shadow-2xl shadow-blue-500/20">
@@ -12,23 +17,49 @@ function Home() {
           You scored higher than 65% of the people who have taken these tests.
         </p>
       </div>
-      <div className=" p-4">
-        <h2>Summary</h2>
-        <ul>
-          <li>
-            <span>Reaction</span> <span></span>80 / 100
+      <div className="flex h-full flex-col justify-between px-11 py-8">
+        <h3 className="font-bold">Summary</h3>
+        <ul className="my-5 flex h-full flex-col justify-between font-bold">
+          <li className="bg-Light-red-transparent flex justify-between rounded-md px-3 py-2 text-[0.8rem]">
+            <div className="flex items-center text-Light-red">
+              <HiOutlineBolt />
+              <span className="ml-2">Reaction</span>
+            </div>
+            <div>
+              80 <span className="text-gray-500">/ 100</span>
+            </div>
           </li>
-          <li>
-            <span>Memory</span> <span>92 / 100</span>
+          <li className="bg-Orangey-yellow-transparent flex justify-between rounded-md px-3 py-2 text-[0.8rem]">
+            <div className="flex items-center text-Orangey-yellow">
+              <LiaBrainSolid />
+              <span className="ml-2">Memory</span>
+            </div>
+            <div>
+              92 <span className="text-gray-500">/ 100</span>
+            </div>
           </li>
-          <li>
-            <span>Verbal</span> <span>61 / 100</span>
+          <li className="bg-Green-teal-transparent flex justify-between rounded-md px-3 py-2 text-[0.8rem]">
+            <div className="flex items-center text-Green-teal">
+              <BiMessageRoundedMinus />
+              <span className="ml-2">Verbal</span>
+            </div>{" "}
+            <span>
+              61 <span className="text-gray-500">/ 100</span>
+            </span>
           </li>
-          <li>
-            <span>Visual</span> <span>72 / 100</span>
+          <li className="bg-Cobalt-blue-transparent flex justify-between rounded-md px-3 py-2 text-[0.8rem]">
+            <div className="flex items-center text-Cobalt-blue">
+              <TbEye />
+              <span className="ml-2">Visual</span>
+            </div>{" "}
+            <div>
+              72 <span className="text-gray-500">/ 100</span>
+            </div>
           </li>
         </ul>
-        <button>Continue</button>
+        <button className="mt-3 rounded-3xl bg-Dark-gray-blue p-3 text-[.75rem] text-Pale-blue hover:bg-Light-royal-blue">
+          Continue
+        </button>
       </div>
 
       {/* <div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -12,9 +12,13 @@ export default {
       colors: {
         // ### Primary
         "Light-red": "hsl(0, 100%, 67%)",
+        "Light-red-transparent": "hsla(0, 100%, 67%,0.1)",
         "Orangey-yellow": "hsl(39, 100%, 56%)",
+        "Orangey-yellow-transparent": "hsl(39, 100%, 56%,0.1)",
         "Green-teal": "hsl(166, 100%, 37%)",
+        "Green-teal-transparent": "hsl(166, 100%, 37%,0.1)",
         "Cobalt-blue": "hsl(234, 85%, 45%)",
+        "Cobalt-blue-transparent": "hsl(234, 85%, 45%,0.1)",
 
         // ## Gradients
         // (background)
